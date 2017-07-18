463c9cbb57925e2d64c7222737c603757091eb00
Patrick Ocampo <pegnovi@yahoo.com>
2017-07-17 18:51:34 -0700

    Got socket.io-client to connect to server's socket.io.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
+const Server = require('socket.io');
+
 const app = require('./app');
 
 const PORT = 9000;
 
 app.listen(PORT, () => {
 	console.log(`App listening on port ${PORT}!`);
+});
+
+const io = new Server().attach(8090);
+io.on('connection', function(socket) {
+	console.log('client connected!');
 });
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:8090');
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
