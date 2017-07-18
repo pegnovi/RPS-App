ac539ba94f3d456b338c7bc1861ee2e1400bfb0e
Patrick Ocampo <pegnovi@yahoo.com>
2017-07-17 18:32:07 -0700

    Added inital Express etc boilerplate.

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// middleware
+// Setup Logger
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version"' + ':status :res[content-length] :response-time ms'));
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// req body middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// backend routing
+//require('./routes.js')(app);
+
+// react: Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
+module.exports = app;
