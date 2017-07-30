6ef8801b189fa1327ffc3c7e534e5ece4a901e89
mcpengelly <pengelly.mat@gmail.com>
2017-07-29 20:00:51 -0400

    [matt-redux-wiring] include redux extension hook

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import {createStore} from 'redux';
 
 import allReducers from './reducers';
 
-const store = createStore(allReducers);
+const store = createStore(
+	allReducers,
+	/* FOR USE WITH REDUX-DEV-TOOLS https://github.com/zalmoxisus/redux-devtools-extension#usage */
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 console.log('INITIAL APP STATE:', store.getState());
 
 registerServiceWorker();
