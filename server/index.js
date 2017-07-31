e92f238bf5e2114c963555686eae9bade9d44eb4
Patrick Ocampo <pegnovi@yahoo.com>
2017-07-31 09:39:36 -0700

    Removed other functions and left boilerplate for sockets.

--- server/index.js
+++ server/index.js
@@ -11,41 +11,10 @@ const server = app.listen(PORT, () => {
 });
 
 
-
-function getRooms(io) {
-	return io.sockets.adapter.rooms;
-}
-
-function getRoomsList(io) {
-	const rooms = getRooms(io);
-	return _.map(rooms, function(room, roomName) {
-		console.log(room);
-		console.log(roomName);
-		return room;
-	});
-}
-
-function startGame(io, socket) {
-	console.log('startGame: ', socket.id);
-
-	// TODO: check if there are any rooms
-	// TODO: check if there are any rooms with less than 2 ppl
-	socket.join('some room', function() {
-		console.log(getRoomsList(io));
-	});
-
-}
-
 //const io = new Server().attach(8090);
 const io = require('socket.io')(server);
 io.on('connection', function(socket) {
 	console.log('client connected!');
-
-
-	socket.on('start game', function() {
-		startGame(io, socket);
-	});
-
 
 	socket.on('error', function(error) {
 		console.log('error: ', error);
