6a3add678087b49686da49d472bbd85eaf29a1a5
Patrick Ocampo <pegnovi@yahoo.com>
2017-08-07 15:26:11 -0700

    Loaded gif onto page.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 import GameContainer from './components/GameContainer';
+
+const imageMap = {
+	rock: {
+		standard: require('./images/rockHandSign.gif'),
+		flipped: require('./images/rockHandSignFlipped.gif')
+	},
+	scissor: {
+		standard: require('./images/scissorHandSign.gif'),
+		flipped: require('./images/scissorHandSignFlipped.gif')
+	},
+	paper: {
+		standard: require('./images/paperHandSign.gif'),
+		flipped: require('./images/paperHandSignFlipped.gif')
+	}
+}
 
 class App extends Component {
 	render() {
+
 		return (
 			<div className="App">
 				<div className="App-header">
@@ -15,6 +32,17 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
+
+				<img id="myImage" src={imageMap['scissor']['standard']}
+				data-animated-source={imageMap['scissor']['standard']}
+				width="360" height="360"
+				data-autoplay="1"
+				alt=""
+				onLoad={() => console.log('LOADED')} />
+
+				{
+					document.getElementById('myImage')
+				}
 
 				<GameContainer />
 
