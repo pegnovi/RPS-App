21ef88dcdd22152dedadab2210bc5e7604f911f6
Patrick Ocampo <pegnovi@yahoo.com>
2017-08-09 18:39:19 -0700

    Deleted gifs since they won't be used.

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import GameContainer from './components/GameContainer';
 
-// const imageMap = {
-// 	rock: {
-// 		standard: require('./images/rockHandSign.gif'),
-// 		flipped: require('./images/rockHandSignFlipped.gif')
-// 	},
-// 	scissor: {
-// 		standard: require('./images/scissorHandSign.gif'),
-// 		flipped: require('./images/scissorHandSignFlipped.gif')
-// 	},
-// 	paper: {
-// 		standard: require('./images/paperHandSign.gif'),
-// 		flipped: require('./images/paperHandSignFlipped.gif')
-// 	}
-// }
-
 class App extends Component {
 	render() {
 
