fad8730321ce7d1e6fb88c388b82f63ea1941422
Patrick Ocampo <pegnovi@yahoo.com>
2017-11-08 21:34:07 -0800

    Disabled parsing urlencoded.

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // parse application/x-www-form-urlencoded
 // for easier testing with Postman or plain HTML forms
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 // req body middleware
 app.use(bodyParser.json());
